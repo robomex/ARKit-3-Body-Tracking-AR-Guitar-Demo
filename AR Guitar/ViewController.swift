30de7f3a4afdb711a6261cf6406e03297898c304
Tony Morales <atmorales@gmail.com>
2019-11-18 23:21:23 -0600

    Initial Commit

---
+++ AR Guitar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AR Guitar
+//
+//  Created by Tony Morales on 11/18/19.
+//  Copyright © 2019 Tony Morales. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
